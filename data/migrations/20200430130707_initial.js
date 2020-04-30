8c735b614b79d87380c2725c684c72cc6abfb0b9
tanveersaleem786 <tanveer.saleem786@gmail.com>
2020-04-30 16:58:38 -0400

    HIT MVP

---
+++ data/migrations/20200430130707_initial.js
@@ -0,0 +1,56 @@
+
+exports.up = async function(knex) {
+
+    await knex.schema.createTable('recipes', (table) => {
+         table.increments("id"),
+         table.text("name").notNull(),
+         table.text("description")
+    })
+
+    await knex.schema.createTable("ingredients", (table) => {
+         table.increments("id"),
+         table.text("name").notNull().unique()
+    })
+
+    await knex.schema.createTable("units", (table) => {
+        table.increments("id"),
+        table.text("name").notNull().unique()
+   })
+
+    await knex.schema.createTable("recipes_ingredients", (table) => {
+         table.integer("recipe_id")
+              .references("id")
+              .inTable("recipes")
+              .onDelete("CASCADE"),
+         table.integer("ingredient_id")
+              .references("id")
+              .inTable("ingredients")
+              .onDelete("CASCADE"),
+         table.float("quantity").notNull(),
+         table.integer("unit_id")
+              .references("id")
+              .inTable("units")
+              .onDelete("CASCADE"),
+         table.primary(["recipe_id","ingredient_id"])
+    }) 
+
+    await knex.schema.createTable("recipe_steps", (table) => {
+         table.increments("id"),
+         table.integer("recipe_id")
+              .references("id")
+              .inTable("recipes")
+              .onDelete("CASCADE"),
+         table.text("step_detail").notNull()
+         table.text("step_order").default(null)
+    })
+  
+};
+
+exports.down = async function(knex) {
+    await knex.schema.dropTableIfExists("recipe_steps")
+    await knex.schema.dropTableIfExists("recipes_ingredients")
+    await knex.schema.dropTableIfExists("units")
+    await knex.schema.dropTableIfExists("ingredients")
+    await knex.schema.dropTableIfExists("recipes")
+  
+};
